277ff6319eeb64c1f9f6558d0fb458fce4e5e4eb
unknownguyoffline <83174004+unknownguyoffline@users.noreply.github.com>
2025-08-15 11:56:57 +0530

    Update install-dependencies.bash

--- install-dependencies.bash
+++ install-dependencies.bash
@@ -1,4 +1,4 @@
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-sudo apt install node npm
+sudo apt install nodejs npm
